cda423dd4168471f26c7257a593ed40075c2beb1
Nolan Dyke <nolan.g.dyke@gmail.com>
2020-02-28 23:31:38 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,9 +9,9 @@ pp directors_database
 def gross_for_director(director_data)
     my_index = 0 
     director_sum = 0
-  binding.pry 
+  #binding.pry 
     while my_index < director_data.length do 
-    #director_sum += director_data[my_index][:worldwide_gross]
+    director_sum += director_data[my_index][:worldwide_gross]
     my_index += 1
   end 
   director_sum
@@ -21,10 +21,10 @@ def directors_totals(nds)
   total_hash = {} 
   array_index = 0 
    while array_index < nds.length do
-    inner_length = nds[array_index][:movies]
+    director_data = nds[array_index][:movies]
     director_sum = 0
     director_name = nds[array_index][:name]
-    director_sum += gross_for_director(inner_length)
+    director_sum += gross_for_director(director_data)
     total_hash[director_name] = director_sum
     array_index +=1 
   end 
